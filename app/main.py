146153ca6a239612e24e8f62e5caf423f62a45cf
Issouffa Ambdouloihi <49631741+Ambdouloihi@users.noreply.github.com>
2025-01-04 20:02:48 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def main():
             case ["exit", "0"]:
                 exit()
             case _:
-                print(f"{' '.join(command)}: command not found")
+                print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
