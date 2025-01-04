3401ad9321c244512b39b32f5782e2f60e161d85
Issouffa Ambdouloihi <49631741+Ambdouloihi@users.noreply.github.com>
2025-01-04 22:27:28 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,49 +1,74 @@
 import os
 import sys
 import shutil
-import subprocess
+from subprocess import run, PIPE
 import shlex
 
 BUILTIN_CMD = {"exit", "echo", "type", "pwd", "cd"}
 
+REDIRECTION_MODES = {
+    ">": ("w", True, None),
+    "1>": ("w", True, None),
+    ">>": ("a", True, None),
+    "1>>": ("a", True, None),
+    "2>": ("w", None, True),
+    "2>>": ("a", None, True),
+}
 
-def type_cmd(command):
+
+def type_cmd(command, stdout, stderr):
     if command in BUILTIN_CMD:
-        print(f"{command} is a shell builtin")
+        print(f"{command} is a shell builtin", file=stdout)
     elif path := shutil.which(command):
-        print(f"{command} is {path}")
+        print(f"{command} is {path}", file=stdout)
     else:
-        print(f"{command}: not found")
+        print(f"{command}: not found", file=stderr)
 
 
-def cd_cmd(path):
+def cd_cmd(path, stderr):
     try:
         os.chdir(os.path.expanduser(path))
     except OSError:
-        print(f"cd: {path}: No such file or directory")
+        print(f"cd: {path}: No such file or directory", file=stderr)
+
+
+def handle_cmd(cmd_args, stdout=None, stderr=None):
+    match cmd_args:
+        case ["exit", "0"]:
+            exit()
+        case ["echo", *args]:
+            print(*args, file=stdout or sys.stdout)
+        case ["type", cmd]:
+            type_cmd(cmd, stdout, stderr)
+        case ["pwd"]:
+            print(os.getcwd(), file=stdout or sys.stdout)
+        case ["cd", pth]:
+            cd_cmd(pth, stderr)
+        case [cmd, *args] if shutil.which(cmd):
+            process = run([cmd, *args], stdout=PIPE, stderr=PIPE, text=True)
+            if process.stdout:
+                print(process.stdout, file=stdout or sys.stdout, end="")
+            if process.stderr:
+                print(process.stderr, file=stderr or sys.stderr, end="")
+        case _:
+            print(f"{' '.join(cmd_args)}: command not found", file=stderr or sys.stderr)
 
 
 def main():
     while True:
         sys.stdout.write("$ ")
-
         command = input()
-
         match shlex.split(command):
-            case ["exit", "0"]:
-                exit()
-            case ["echo", *args]:
-                print(*args)
-            case ["type", cmd]:
-                type_cmd(cmd)
-            case ["pwd"]:
-                print(os.getcwd())
-            case ["cd", pth]:
-                cd_cmd(pth)
-            case [cmd, *args] if shutil.which(cmd):
-                subprocess.run([cmd, *args])
-            case _:
-                print(f"{command}: command not found")
+            case [*cmd_args, op, file] if op in REDIRECTION_MODES:
+                mode, capture_stdout, capture_stderr = REDIRECTION_MODES[op]
+                with open(file, mode) as f:
+                    handle_cmd(
+                        cmd_args,
+                        stdout=f if capture_stdout else None,
+                        stderr=f if capture_stderr else None,
+                    )
+            case cmd_args:
+                handle_cmd(cmd_args)
 
 
 if __name__ == "__main__":
